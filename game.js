49c14a79ec6e69a4c2ab37a6026f97c4d64a2253
mav-zate <maverick.alzate@gmail.com>
2017-09-06 17:41:41 -0400

    Make mario move let and right

--- game.js
+++ game.js
@@ -1,45 +1,67 @@
 var brick = new Image();
-var mario = new Image();
 
 const BRICK_LENGTH = 40;
+const CANVAS_HEIGHT = 500;
+const CANVAS_WIDTH = 800;
+
+let mario = {
+  xPos: 2 * BRICK_LENGTH,
+  yPos: CANVAS_HEIGHT - (3 * BRICK_LENGTH),
+  image: new Image(),
+};
 
 const initImages = () => {
   brick.src = "https://s3.amazonaws.com/running-mario-bros-dev/env/8bit_brick.png";
-  mario.src = "https://s3.amazonaws.com/running-mario-bros-dev/mario/standing.png";
+  mario.image.src = "https://s3.amazonaws.com/running-mario-bros-dev/mario/standing.png";
 };
 
 const buttonHandler = (e) => {
-  if (e.key === "ArrowRight") {
-    
-  } else if (e.key === "ArrowLeft") {
-    console.log("I'm working left, lol");
+  switch (e.key) {
+    case "ArrowRight":
+      console.log('right');
+      marioMover(1, 0);
+      break;
+    case "ArrowLeft":
+      marioMover(-1, 0);
+      console.log('left');
+      break;
+    default:
+      return;
   }
 };
 
+const marioMover = (dx, dy) => {
+  mario.xPos += dx * 10;
+  mario.yPos += dy * 10;
+};
+
 const render = () => {
-  initImages();
   const canvas = document.getElementById('game-screen');
   const context = canvas.getContext('2d');
+
+  context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
   // draw bricks
   for (let y = 0; y < 2; y++) {
     for (let x = 0; x < 20; x++) {
       context.drawImage(brick,
-        x * BRICK_LENGTH, canvas.height - (2 * BRICK_LENGTH) + (y * BRICK_LENGTH),
+        x * BRICK_LENGTH, CANVAS_HEIGHT - (2 * BRICK_LENGTH) + (y * BRICK_LENGTH),
         BRICK_LENGTH, BRICK_LENGTH);
     }
   }
 
   // draw mario
-  context.drawImage(mario,
-    2 * BRICK_LENGTH, canvas.height - (3 * BRICK_LENGTH),
+  context.drawImage(mario.image,
+    mario.xPos, mario.yPos,
     3 * BRICK_LENGTH / 4, BRICK_LENGTH);
 };
 
 
+/******************     MAIN SCRIPT   *****************************/
+
 window.addEventListener('keydown', e => (buttonHandler(e)));
 
 document.addEventListener('DOMContentLoaded', () => {
-  // make mario move right
+  initImages();
   window.setInterval(render, 17);
 });
